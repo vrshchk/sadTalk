57f53062fa8c22ccc19097281dc13bed0c2cca35
Yelyzaveta Boiarchuk <liz.bk06@gmail.com>
2020-09-20 15:25:19 +0300

    comm

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import telebot
 import config
+import time
+import pymongo
+
+
+myclient = pymongo.MongoClient("mongodb+srv://user:userpass@sadtalk.0ycvj.mongodb.net/patternsDB?retryWrites=true&w=majority")
+mydb = myclient["patternsDB"]
+
+print(myclient.list_database_names())
+
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-	s = message.text+'!!'
+	#bot.send_chat_action(chat_id=message.chat.id, action = Telegram.chat.ChatAction.TYPING)
+	time.sleep(1)
+	s = message.text+'!!!!'
 	bot.send_message(message.chat.id, s)
 
 
-bot.polling(none_stop=True)
+
+bot.polling(none_stop=True, interval=0, timeout=20)
+
