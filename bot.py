530655e55bd0101aeea08799c80b33a2427e1f22
Yelyzaveta Boiarchuk <liz.bk06@gmail.com>
2020-09-20 21:11:08 +0300

    chat action added + intents

--- bot.py
+++ bot.py
@@ -10,10 +10,16 @@ mydb = myclient["patternsDB"]
 
 collist = mydb.list_collection_names()
 collection = mydb.intents
+index_cursor = collection.list_indexes()
+print(index_cursor)
 
+
+for index in index_cursor:
+	print(index.keys())
 
 for post in collection.find():
-	print(post)
+	arr = (post['patterns'])
+	print(arr)
 
 
 
@@ -26,10 +32,31 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-	#bot.send_chat_action(chat_id=message.chat.id, action = Telegram.chat.ChatAction.TYPING)
+	bot.send_chat_action(chat_id=message.chat.id, action = 'typing')
 	time.sleep(1)
-	s = message.text+'!!!!'
-	bot.send_message(message.chat.id, s)
+	s = message.text
+	ans = ':)' 
+	answerFound = 0
+ 
+	foundExactly = (collection.find({"patterns":s}).count())
+
+	if (foundExactly != 0 ):
+		ans = collection.find_one({"patterns":s})["answers"]
+		answerFound = 1
+
+	if (answerFound==0):
+		for obj in collection.find():
+			arr = obj["patterns"]
+			for answ in arr:
+				if (answ in s):
+					temp = obj['answers']
+					ans = temp[0]
+					ans = ans.replace("*",answ)
+
+
+		
+	
+	bot.send_message(message.from_user.id,ans)
 
 
 
