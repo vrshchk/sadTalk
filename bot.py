5bfc02a4e74c66eb3a97b904ada6f6334741caea
vrshchk <maryvaresh12@gmail.com>
2020-09-22 18:30:57 +0300

    final

--- bot.py
+++ bot.py
@@ -10,45 +10,35 @@ with open('intents.json') as file:
 
 bot = telebot.TeleBot(config.TOKEN)
 
-
-
-
-
-
-
 def randChoice(arr):
     rand = numpy.random.randint(0, len(arr))
     return arr[rand]
 
-
 def findAnswer(s):
-
-    for intent in data['intents']:
-        doc = intent
-        patterns = doc["patterns"]
-        for pattern in patterns:
-            if (pattern in s):
-                if ( doc["context_set"] in s):
-                    if ( doc["anti_context_set"] != "-"):
-                        if (not doc["anti_context_set"] in s):
-                            answers = doc["answers"]
-                            result = randChoice(answers)
-                            result = result.replace("*", pattern)
-                            return result
-                           
+	if (s=="/start"):
+		return "Chat started"
+	else:
+	    for intent in data['intents']:
+	        doc = intent
+	        patterns = doc["patterns"]
+	        for pattern in patterns:
+	            if (not re.search( ("[a-zA-Z]"+pattern),s) or pattern==""):
+	                if (not re.search((pattern+"[a-zA-Z]"),s) or pattern==""):
+	                    if (pattern in s):
+	                        if ( doc["context_set"] in s):
+	                            if ( doc["anti_context_set"] != "-"):
+	                                if (not doc["anti_context_set"] in s):
+	                                    answers = doc["answers"]
+	                                    result = randChoice(answers)
+	                                    result = result.replace("*", pattern)
+	                                    return result
+	                            else:
+	                                answers = doc["answers"]
+	                                result = randChoice(answers)
+	                                result = result.replace("*", pattern)
+	                                return result
         
-
-
-
-
-
-
-
-        
-
-
-
-
+   
 @bot.message_handler(content_types=['text'])
 def lalala(message):
   bot.send_chat_action(chat_id=message.chat.id, action = 'typing')
